435db8e5def45831cfd2e7918f176548ff95f405
thelioncrazy51 <67366172+thelioncrazy51@users.noreply.github.com>
2025-07-01 04:15:50 +0700

    Create products.blade.php

---
+++ resources/views/pages/@extends('main.layout')  @section('title', 'Produk')  @section('container') <div class="glass-card">     <h1>Daftar Produk</h1>     <!-- Daftar produk --> </products.blade.php
@@ -0,0 +1,10 @@
+@extends('main.layout')
+
+@section('title', 'Produk')
+
+@section('container')
+<div class="glass-card">
+    <h1>Daftar Produk</h1>
+    <!-- Daftar produk -->
+</div>
+@endsection
