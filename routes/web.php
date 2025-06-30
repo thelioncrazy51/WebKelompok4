66fc575f358736246b54d1927c2b1688f6d67135
thelioncrazy51 <67366172+thelioncrazy51@users.noreply.github.com>
2025-07-01 06:00:21 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HalamanController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DataController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DataController;
 
 // Halaman Home
 Route::get('/', function () {
