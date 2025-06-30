c5a78e279850d387d1dc6b2e89b598e25756e1fe
thelioncrazy51 <67366172+thelioncrazy51@users.noreply.github.com>
2025-07-01 06:52:59 +0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,11 +8,17 @@ class DashboardController extends Controller
 {
     public function memberDashboard()
     {
-        return view('pages.dashboard.member', ['title' => 'Member Dashboard']);
+        return view('pages.dashboard.member', [
+            'title' => 'Member Dashboard',
+            'user' => auth()->user()
+        ]);
     }
 
     public function adminDashboard()
     {
-        return view('pages.dashboard.admin', ['title' => 'Admin Dashboard']);
+        return view('pages.dashboard.admin', [
+            'title' => 'Admin Dashboard',
+            'user' => auth()->user()
+        ]);
     }
 }
