f23585a6a2e6053e1c85e563fc6dcef8096e3009
thelioncrazy51 <67366172+thelioncrazy51@users.noreply.github.com>
2025-07-01 05:22:52 +0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,12 +6,13 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    // DashboardController.php
-    public function index()
+    public function memberDashboard()
     {
-        if (Auth::user()->role === 'admin') {
-            return view('pages.dashboard.admin');
-        }
-        return view('pages.dashboard.member');
+        return view('dashboard.member', ['title' => 'Member Dashboard']);
+    }
+
+    public function adminDashboard()
+    {
+        return view('dashboard.admin', ['title' => 'Admin Dashboard']);
     }
 }
