90c9f65207ecf58e24ab5485ba968ac1c964d5c1
thelioncrazy51 <67366172+thelioncrazy51@users.noreply.github.com>
2025-07-01 04:15:14 +0700

    Create harvest_prediction.blade.php

---
+++ resources/views/pages/harvest_prediction.blade.php
@@ -0,0 +1,10 @@
+@extends('main.layout')
+
+@section('title', 'Prediksi Panen')
+
+@section('container')
+<div class="glass-card">
+    <h1>Prediksi Panen</h1>
+    <!-- Form/tabel prediksi panen -->
+</div>
+@endsection
