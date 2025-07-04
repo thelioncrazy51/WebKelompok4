02bf787edd6a78195948bcfeb4d2b1fcba84c55c
thelioncrazy51 <67366172+thelioncrazy51@users.noreply.github.com>
2025-07-04 23:15:13 +0700

    Update layout.blade.php

--- resources/views/main/layout.blade.php
+++ resources/views/main/layout.blade.php
@@ -6,6 +6,7 @@
     <title>Halaman {{ $title }} </title>
     {{-- bs cdn --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     {{-- js cdn --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
