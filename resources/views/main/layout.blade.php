f128ca23f4387712880f24f4adec11f93ffbe55d
thelioncrazy51 <67366172+thelioncrazy51@users.noreply.github.com>
2025-07-04 23:20:55 +0700

    Update layout.blade.php

--- resources/views/main/layout.blade.php
+++ resources/views/main/layout.blade.php
@@ -145,7 +145,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         // Toggle sidebar untuk mobile
         function toggleSidebar() {
