f1346eb0ca530128f7ee95dfbecc1cdc62268b70
Muhammad Tsaqif <aandeluxee@gmail.com>
2025-07-06 13:48:15 +0700

    Update harvest_prediction.blade.php

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -5,8 +5,10 @@ namespace App\Exports;
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class UsersExport implements FromCollection, WithHeadings
+class UsersExport implements FromCollection, WithHeadings, WithStyles
 {
     public function collection()
     {
@@ -21,6 +23,22 @@ class UsersExport implements FromCollection, WithHeadings
             'Email',
             'Password',
             'Role'
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            // Style untuk header (baris pertama)
+            1 => [
+                'font' => ['bold' => true, 'color' => ['rgb' => 'FFFFFF']],
+                'fill' => ['fillType' => 'solid', 'startColor' => ['rgb' => '4CAF50']]
+            ],
+
+            // Style untuk seluruh data
+            'A:D' => [
+                'alignment' => ['horizontal' => 'center']
+            ]
         ];
     }
 }
