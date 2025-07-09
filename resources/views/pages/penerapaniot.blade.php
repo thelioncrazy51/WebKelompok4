70345aeff34ca18e44848fd5c8e3155e5099edeb
thelioncrazy51 <67366172+thelioncrazy51@users.noreply.github.com>
2025-07-09 15:50:28 +0700

    Update penerapaniot.blade.php

--- resources/views/pages/penerapaniot.blade.php
+++ resources/views/pages/penerapaniot.blade.php
@@ -2,8 +2,6 @@
 
 @section('body-attr', 'style=background:linear-gradient(135deg, #0b3d0b, #a7d7a7);')
 
-@section('title', 'Penerapan IoT dalam Pertanian')
-
 @section('container')
 <style>
     .glass-card {
